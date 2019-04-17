28752f31058f52fb13812535ea49276a4665b29d
Wenjie Li <wenjie.li516@gmail.com>
2019-04-17 11:24:22 -0400

    detail page 4/17

---
+++ views/script-detail-page.js
@@ -0,0 +1,8 @@
+
+
+// Click on search button to show search bar
+var searchIcon = document.getElementById("navbar-search-icon");
+var searchBar = document.getElementById("navbar-searchbar");
+
+searchIcon.onclick = function buttonOnClick() {
+    searchBar.style.display = "inline-block"; };
