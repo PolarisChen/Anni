9205aede6514dcfe493573559d0ee610df6dd10f
PolarisChen <polaris_chen@163.com>
2019-04-17 10:31:36 -0400

    Add user routes

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,20 @@ router.get('/addUser', function(req, res, next) {
   userDao.add(req, res, next);
 });
 
+router.get('/queryAll', function(req, res, next) {
+  userDao.queryAll(req, res, next);
+});
+
+router.get('/query', function(req, res, next) {
+  userDao.queryById(req, res, next);
+});
+
+router.get('/deleteUser', function(req, res, next) {
+  userDao.delete(req, res, next);
+});
+
+router.post('/updateUser', function(req, res, next) {
+  userDao.update(req, res, next);
+});
+
 module.exports = router;
