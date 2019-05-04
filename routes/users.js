2cc930bd9e461a266c1d52f067ffa8fb98eece73
PolarisChen <polaris_chen@163.com>
2019-05-04 15:41:10 -0400

    Update users routes

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,14 @@ const sessionChecker = require('../middleware/sessionChecker');
 // Fetch user data with id
 router.get('/', sessionChecker, (req, res, next) => {
   userService.getUser(req.session.userId).then((ret)=>{
-    res.json(ret);
-    // res.render('user', ret.data);
+    // res.json(ret);
+    res.render('user', ret.data);
   });
 });
 router.get('/:userId', (req, res, next) => {
   userService.getUser(req.params.userId).then((ret)=>{
-    res.json(ret);
-    // res.render('user', ret.data);
+    // res.json(ret);
+    res.render('user', ret.data);
   });
 });
 
