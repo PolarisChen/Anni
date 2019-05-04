ded02b065173423b505c604f6b00c87f3ba63f2e
PolarisChen <polaris_chen@163.com>
2019-05-04 10:52:41 -0400

    Add user name

--- dao/commentSqlMapping.js
+++ dao/commentSqlMapping.js
@@ -3,7 +3,7 @@ let comment = {
   insert: 'INSERT INTO comments(content, parentId, userId, anniId) VALUES(?,?,?,?)',
   update: 'UPDATE comments set content=? WHERE id=?',
   delete: 'DELETE FROM comments WHERE id=?',
-  queryByAnniId: 'SELECT * FROM comments WHERE anniId=?',
+  queryByAnniId: 'SELECT c.*, u.name AS userName FROM comments c, users u WHERE c.anniId=? AND c.userId=u.id',
   insertLike: 'INSERT INTO comment_likes(commentId, userId) VALUES(?,?)',
   deleteLike: 'DELETE FROM comment_likes WHERE id=?',
   queryLike: 'SELECT COUNT(*) FROM comment_likes WHERE commentId=?',
